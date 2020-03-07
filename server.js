15bafed656756bf565442a24f1934a34bfabce1d
luistkd4 <33152877+luistkd4@users.noreply.github.com>
2020-03-07 17:32:56 -0300

    add hostname

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+const os = require('os');
 app.get('/', function (req, res) {
-res.send('Hello World!');
+res.send(`Hello World!\n<h3>HOST: ${os.hostname()}</h3>`);
 });
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
