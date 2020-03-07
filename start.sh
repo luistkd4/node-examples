57366ec8fa286a7be54007ef568c8775347dc249
luistkd4 <33152877+luistkd4@users.noreply.github.com>
2020-03-07 18:10:49 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-service=`docker service ls -f NAME=node-app | wc -l`
-
-if [[ service -gt 1 ]];then
+#!/bin/bash
+if [[ `docker service ls -f NAME=node-app | wc -l` -gt 1 ]];then
         docker service update --image nodeapp:$TAG node-app;
 else
         docker service create --name node-app -p 8081:3000 nodeapp:$TAG
