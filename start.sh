d0389f0f4881563bd7e449fc2aca7102b602c920
luistkd4 <33152877+luistkd4@users.noreply.github.com>
2020-03-07 18:16:39 -0300

    add replicas parameter

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ `docker service ls -f NAME=node-app | wc -l` -gt 1 ]];then
-        docker service update --image nodeapp:$TAG node-app;
+        docker service update --image nodeapp:$TAG --replicas $PROC node-app;
 else
         docker service create --name node-app -p 8081:3000 nodeapp:$TAG
 fi
