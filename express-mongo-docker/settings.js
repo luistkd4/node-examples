8db788a8f66521997b71a42c3871656b8552b452
Luis Miguel <miguel@umbler.com>
2020-06-28 20:16:24 -0300

    set uppercase at vars

--- express-mongo-docker/settings.js
+++ express-mongo-docker/settings.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 var util = require('util');
 
 // Set database vars
-var db_user = process.env["database-user"];
-var db_pass = process.env["database-password"];
-var db_host_name = process.env["database-host-name"];
-var db_port = process.env["database-port"];
-var db_name = process.env["database-name"];
+var db_user = process.env.DATABASE_USER;
+var db_pass = process.env.DATABASE_PASSWORD;
+var db_host_name = process.env.DATABASE_HOST_NAME;
+var db_port = process.env.DATABASE_PORT;
+var db_name = process.env.DATABASE_NAME;
 
 const DB_CONFIG = util.format('mongodb://%s:%s@%s:%s/%s', db_user, db_pass, db_host_name, db_port, db_name);
 
